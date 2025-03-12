18821ec44451581f153a6a39373d72e49263f2ab
Kynan <n11477504@qut.edu.au>
2025-03-13 06:19:07 +1000

    Tested FullTimeEmployee class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-
+        FullTimeEmployee employee1 = new FullTimeEmployee("jay", "1", 100000);
+        System.out.println(employee1.calculateSalary());
     }
 }
