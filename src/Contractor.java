700210bd3687306a153a808a3dd5abcac974ce64
Kynan <n11477504@qut.edu.au>
2025-03-13 13:07:03 +1000

    Contractor complete

--- src/Contractor.java
+++ src/Contractor.java
@@ -1,2 +1,24 @@
-public class Contractor {
+/**
+ * Attributes: contract_rate, projects_completed
+ * Methods: calculateSalary - based on number of projects completed by contract rate
+ */
+public class Contractor extends Employee {
+    private double contract_rate;
+    private double projects_completed;
+
+    public Contractor(String name, String id) {
+        super(name, id);
+    }
+
+    public double getContractRate() { return this.contract_rate; }
+    public double getProjectsCompleted() { return this.projects_completed; }
+
+    public void setContractRate(double contract_rate) { this.contract_rate = contract_rate; }
+    public void setProjectsCompleted(double projects_completed) { this.projects_completed = projects_completed; }
+
+    @Override
+    public double calculateSalary() {
+        return contract_rate * projects_completed;
+    }
+
 }
